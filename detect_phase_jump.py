3e8d58b9599dc771cbc3444bb1ab6c48baccacf4
porkbellie <ellie.han@temple.edu>
2025-07-07 11:47:39 -0400

    Update detect_phase_jump.py

--- detect_phase_jump.py
+++ detect_phase_jump.py
@@ -2,6 +2,9 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# DETECT_PHASE_JUMP: detects a phase jump and corrects it and returns the smoothed array of phases
+# inputs: phi, cutoff
+# output: phi_smooth
 def detect_phase_jump(phi, cutoff):
 
     n = length of phi
@@ -25,6 +28,19 @@ def detect_phase_jump(phi, cutoff):
 
 return phi_smooth
 
-def integrate_smooth(phi_smooth):
-    
+# DERIVATIVE_SMOOTH: takes the derivative of smoothed phases from DETECT_PHASE_JUMP and returns
+# inputs: omega, phi_smooth
+# output: derivative 
+def derivative_smooth(omega, phi_smooth):
+    n = len(phi_smooth) #length of phi_smooth
+    derivative = np.zeros(n) #array of length n
+
+    derivative[0] = (phi_smooth[1] - phi_smooth[0]) / (omega[1] - omega[0]) #forward diff at start
+
+    for i in range(1, n-1): #central diff
+        derivative[i] = (phi_smooth[i + 1] - phi_smooth[i - 1]) / (omega[i + 1] - omega[i - 1])
+
+    derivative[n - 1] = (phi_smooth[n - 1] - phi_smooth[n - 2]) / (omega[n - 1] - omega[n - 2]) #backward diff at end
+
+return derivative
     
